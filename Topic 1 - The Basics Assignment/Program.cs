db90f4a2a6df63dd9336bdb9bb02ca96980a0943
natebarnes123 <barnesn261@gmail.com>
2024-09-11 08:47:33 -0400

    Update Program.cs

--- Topic 1 - The Basics Assignment/Program.cs
+++ Topic 1 - The Basics Assignment/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Topic_1___The_Basics_Assignment
+namespace Topic_1___The_Basics_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   // Nate Barnes
+            Console.WriteLine("Hello, I am Nate Barnes");
+          
+            Console.WriteLine("Comments explain what a section of non obvious code does and can be used to guide other through your own code.");
+            Console.WriteLine();
+            // This program spells out my initials.
+            Console.WriteLine("N    N   BBBBB ");
+            Console.WriteLine("NN   N   B    B ");
+            Console.WriteLine("N N  N   B    B");
+            Console.WriteLine("N   NN   BBBBB ");
+            Console.WriteLine("N    N   B    B");
+            Console.WriteLine("N    N   B    B");
+            Console.WriteLine("N    N   BBBBB");
+            
         }
+
+
+
     }
 }
